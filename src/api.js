fe887f0cde89000ba54ac817c4e53fa64f6899e4
zerose <zerose219@gmail.com>
2026-01-06 16:50:54 +0900

    API 주소 수정

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000'
+  baseURL: import.meta.env.VITE_API_BASE || '/api'
 })
 
 export async function fetchUsers(){
-  const { data } = await api.get('/api/users')
+  const { data } = await api.get('/users')
   return data
 }
 
 export async function fetchRecommendations(userId, limit=12){
-  const { data } = await api.get('/api/recommend', { params: { user_id: userId, limit } })
+  const { data } = await api.get('/recommend', { params: { user_id: userId, limit } })
   return data
 }
 
 export async function fetchMovies(){
-  const { data } = await api.get('/api/movies?limit=100')
+  const { data } = await api.get('/movies?limit=100')
   return data
 }
 
